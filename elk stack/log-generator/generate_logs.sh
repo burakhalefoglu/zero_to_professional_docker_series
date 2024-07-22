c66b479a7920186ade0efe695e71d0063cc15478
Burak Halefoğlu <burakhalefoglu@gmail.com>
2024-07-22 22:20:03 +0300

    rf

---
+++ elk stack/log-generator/generate_logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+generate_log() {
+  local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  local user_id=$((RANDOM % 1000 + 1))
+  local product_id=$((RANDOM % 100 + 1))
+  local quantity=$((RANDOM % 10 + 1))
+  local price=$(awk -v min=10 -v max=100 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+  local total=$(awk "BEGIN {print $quantity * $price}")
+
+  echo "$timestamp, user_id: $user_id, product_id: $product_id, quantity: $quantity, price: $price, total: $total"
+}
+
+while true; do
+  log=$(generate_log)
+  echo "$log" | nc logstash 5000
+  sleep 1
+done
